c00644f7922e2ca03b00385b4f04c2ca3dde3c6b
Daswetter <67914852+Daswetter@users.noreply.github.com>
2020-10-19 22:36:07 +0300

    rate button - svg

---
+++ src/blocks/rate-button/rate-button.js
@@ -0,0 +1,6 @@
+import '../../../node_modules/jquery.rateit/scripts/jquery.rateit.js'
+import '../../../node_modules/jquery.rateit/scripts/jquery.rateit.min.js'
+
+jQuery(function($){
+    $(".rateit").rateit()
+});
