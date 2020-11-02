e7453f6c70789bb6c90a9ab24939904bccf1e99a
Daswetter <67914852+Daswetter@users.noreply.github.com>
2020-11-02 22:04:36 +0300

    button apply in calendar

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,30 +1,51 @@
-import 'air-datepicker/dist/js/datepicker.min.js';
+import 'air-datepicker/dist/js/datepicker.js';
 
 $(function() {
+  // datepicker.prototype = {
+  // init: function () {
+  //   this.$datepicker.on('mouseenter', '.datepicker--cell', this._onMouseEnterCell.unbind(this));
+  //   this.$datepicker.on('mouseleave', '.datepicker--cell', this._onMouseLeaveCell.unbind(this));
+  //   }}
+  let $start = $('.calendar__start'),
+      $end   = $('.calendar__end');
 
-  let $start = $('.start'),
-      $end   = $('.end');
-
-  let picker = $('.start').datepicker({
+  let picker = $('.calendar__start').datepicker({
     range: true,
     multipleDatesSeparator: '-',
     language: "en",
     clearButton: true,
-    todayButton: true,
+    // todayButton: true,
+    // applyButton:true,
     classes: 'abs',
     prevHtml:'<span class="material-icons">arrow_back</span>',
     nextHtml:'<span class="material-icons">arrow_forward</span>',
 
+    
     onSelect: function (fd, d, picker) { 
-      $(".start").val(fd.split("-")[0]);
-      $(".end").val(fd.split("-")[1]);
+      $(".calendar__start").val(fd.split("-")[0]);
+      $(".calendar__end").val(fd.split("-")[1]);
     },
     onHide: function() {
       console.log(this);
+    },
+    onShow(inst, animationCompleted) {
+      if (!animationCompleted) {
+        if (!inst.$datepicker.find('.datepicker--buttons .datepicker--button-apply').length) {
+          $('<span class="datepicker--button datepicker--button-apply">Применить</span>').on('click', function (e) {
+            e.stopPropagation();
+            inst.hide();
+          }).appendTo(inst.$datepicker.find('.datepicker--buttons'));
+        }
+      }
     }
   }).data('datepicker');
 
-  $('.end').on('click', () => {
+  $('.calendar__end').on('click', () => {
     picker.show();
   });
+  
 });
+
+
+
+  
