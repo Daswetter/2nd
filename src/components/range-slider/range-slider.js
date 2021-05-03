1698c7c1d591e49f3395b47bb64741720dd3f1ac
Daswetter <daswetter@yandex.ru>
2021-05-03 15:49:58 +0300

    fixed range slider

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,25 +1,20 @@
-import ionRangeSlider from 'ion-rangeslider';
+import ionRangeSlider from 'ion-rangeslider'
 import './range-slider.scss'
 
+const showCurrent = function (data) {
+  const current = document.querySelector('.range-slider__current')
+  current.innerText = data.from_pretty + '₽' + ' - ' + data.to_pretty + '₽'
+}
+
 $(".js-range-slider").ionRangeSlider({
-    type: "double",
-    // skin: "round",
-    min: 0,
-    max: 15000,
-    from: 5000,
-    to: 10000,
-    onChange: function (data) {
-        console.dir(data);
-    },
-    hide_min_max:true,
-    hide_from_to:true
+  type: "double",
+  min: 0,
+  max: 15000,
+  from: 5000,
+  to: 10000,
+  hide_min_max: true,
+  prettify_separator: ' ',
+  onChange: showCurrent,
+  onStart: showCurrent,
 });
 
-// $d4.on("change", function () {
-//     var $inp = $(this);
-//     var v = $inp.prop("value");     // input value in format FROM;TO
-//     var from = $inp.data("from");   // input data-from attribute
-//     var to = $inp.data("to");       // input data-to attribute
-
-//     console.log(from, to);       // all values
-// });
