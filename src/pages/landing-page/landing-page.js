07f96804d690f72982063d642dc2f3f9372d89ff
Daswetter <daswetter@yandex.ru>
2021-05-08 00:10:26 +0300

    refactoring registration page

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -2,4 +2,7 @@ import './landing-page.scss'
 
 const headerHeight = document.querySelector('.header').offsetHeight
 const pageSizeWithoutScroll = document.querySelector('.landing-page__main')
-pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
+if (pageSizeWithoutScroll) {
+  pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
+}
+
---
+++ src/pages/registration-page/registration-page.js
@@ -0,0 +1,8 @@
+import './registration-page.scss'
+
+// const headerHeight = document.querySelector('.header').offsetHeight
+// const pageSizeWithoutScroll = document.querySelector('.registration-page')
+// if (pageSizeWithoutScroll) {
+//   pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
+// }
+
