befb2fbe2485166870487a5e7a5743f337ef1b90
Daswetter <daswetter@yandex.ru>
2021-05-07 23:00:28 +0300

    refactoring headers and footers, landing page

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,1 +1,5 @@
 import './landing-page.scss'
+
+const headerHeight = document.querySelector('.header').offsetHeight
+const pageSizeWithoutScroll = document.querySelector('.landing-page__main')
+pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
