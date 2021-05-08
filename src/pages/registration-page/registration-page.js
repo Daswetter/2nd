3e597c06c4b7724e55828ab5d59403302aa482fa
Daswetter <daswetter@yandex.ru>
2021-05-08 17:10:27 +0300

    page refactoring

--- src/pages/registration-page/registration-page.js
+++ src/pages/registration-page/registration-page.js
@@ -1,8 +1,2 @@
 import './registration-page.scss'
 
-// const headerHeight = document.querySelector('.header').offsetHeight
-// const pageSizeWithoutScroll = document.querySelector('.registration-page')
-// if (pageSizeWithoutScroll) {
-//   pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
-// }
-
