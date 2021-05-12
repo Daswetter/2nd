9d2dd81aa4d9fb7b066fa19f9ebfb72f80d85433
Daswetter <67914852+Daswetter@users.noreply.github.com>
2021-05-12 12:59:47 +0300

    appearance fixing

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,8 +1,12 @@
 import './landing-page.scss'
 
-const headerHeight = document.querySelector('.header').offsetHeight
-const pageSizeWithoutScroll = document.querySelector('.landing-page__main')
-if (pageSizeWithoutScroll) {
-  pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
+const header = document.querySelector('.header')
+if (header){
+  const headerHeight = header.offsetHeight
+  const pageSizeWithoutScroll = document.querySelector('.landing-page__main')
+  if (pageSizeWithoutScroll) {
+    pageSizeWithoutScroll.style.height = window.innerHeight - headerHeight + 'px'
+  }
 }
+
 
