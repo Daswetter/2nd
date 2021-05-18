ac1d6e3bbee0045df3a02c0351314213b74f1317
Daswetter <67914852+Daswetter@users.noreply.github.com>
2021-05-18 10:27:37 +0300

    calendar refactoring

--- src/components/calendar/calendar_constant/calendar_constant.js
+++ src/components/calendar/calendar_constant/calendar_constant.js
@@ -16,17 +16,7 @@ $(function() {
       days: 'MM yyyy',
     },
     
-    dateFormat: 'd M',
-
-    onShow(inst, animationCompleted) {
-      if (!animationCompleted) {
-        if (!inst.$datepicker.find('.datepicker--buttons .datepicker--button-apply').length) {
-          $('<span class="datepicker--button datepicker--button-apply">Применить</span>').on('click', function (e) {
-            e.stopPropagation();
-            inst.hide();
-          }).appendTo(inst.$datepicker.find('.datepicker--buttons'));
-        }
-      }
-    },
+  
   });
+  $('.datepicker--buttons').append('<span class="datepicker--button datepicker--button-apply">Применить</span>')
 })
