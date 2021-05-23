631a61f586c73a54599a8f05651ebb3c22549c02
Daswetter <67914852+Daswetter@users.noreply.github.com>
2021-05-23 22:55:58 +0300

    added js class to range slider

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,19 +1,34 @@
 import ionRangeSlider from 'ion-rangeslider'
 
-const showCurrent = function (data) {
-  const current = document.querySelector('.range-slider__current')
-  current.innerText = data.from_pretty + '₽' + ' - ' + data.to_pretty + '₽'
+class RangeSlider {
+  constructor($component) {
+    this.$component = $component
+    this.init()
+  }
+
+  init() {
+    $(this.$component).ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 15000,
+      from: 5000,
+      to: 10000,
+      hide_min_max: true,
+      prettify_separator: ' ',
+      onChange: this.showCurrent,
+      onStart: this.showCurrent,
+    });
+  }
+
+  showCurrent(data) {
+    const $current = document.querySelector('.js-range-slider__current', this.$component)
+    $current.innerText = data.from_pretty + '₽' + ' - ' + data.to_pretty + '₽'
+  }
 }
 
-$(".js-range-slider").ionRangeSlider({
-  type: "double",
-  min: 0,
-  max: 15000,
-  from: 5000,
-  to: 10000,
-  hide_min_max: true,
-  prettify_separator: ' ',
-  onChange: showCurrent,
-  onStart: showCurrent,
+$(() => {
+  $('.js-range-slider').each((index, node) => {
+    new RangeSlider($(node));
+  });
 });
 
